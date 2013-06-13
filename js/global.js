b4c1cedbf3fda46a0ee74bc8f81982bc83292608
Morten Noddeland <morten@noddeland.no>
2013-06-13 19:50:24 +0200

    Changed to using Google CDN to host jQuery

--- js/global.js
+++ js/global.js
@@ -1,78 +1,80 @@
-function css_flip(){
-  $('.flip-button').click(function(){
-    var card = $(this).parent().parent();
-    if (card.hasClass('flipped')){
-      card.removeClass('flipped');
-    }
-    else {
-      $('.flipped').removeClass('flipped');
-      card.addClass('flipped');
-    }
-    return false;
-  });
-}
-
-function jquery_flip(){
-  var box_width = $('.linkbox').width();
-  var margin = box_width / 2 + 'px';
-  box_width += 'px';
-
-  var compress = {
-      width: 0,
-      marginLeft: margin,
-      opacity: 0.4
-  };
-  var decompress = {
-    width: box_width,
-    marginLeft: 0,
-    opacity: 1
-  };
-
-  var css_properties = {
-    "compress":compress,
-    "decompress":decompress
-  };
-
-  $(".back").css(compress);
-  $(".back").hide();
-
-  //animate width to 0 and margin-top to 1/2 width
-  $('.flip-button').click(function(){
-    var card = $(this).parent();
-    if (card.hasClass('back')) {
-      animate_jquery_flip($('.back:visible'), css_properties);
-    }
-    else {
-      animate_jquery_flip($('.back:visible'), css_properties);
-      animate_jquery_flip(card, css_properties);
-    }
-  });
-}
-
-function animate_jquery_flip(card, css_properties) {
-  card.animate(css_properties["compress"], 100, function() {
-    $(this).hide();
-    // animate second card to full width and margin-top to 0
-    $(this).siblings('.linkbox').show().animate(css_properties["decompress"], 100);
-  });
-}
-
-// Add "hover" to the linkbox in focus
-$(".no-touch .front").hover(
-  function(){
-    $(this).addClass("hover");
-    $(this).find(".flip-button").show();
-  },
-  function(){
-    $(this).removeClass("hover");
-    $(this).find(".flip-button").hide();
+$(function() {
+  function css_flip(){
+    $('.flip-button').click(function(){
+      var card = $(this).parent().parent();
+      if (card.hasClass('flipped')){
+        card.removeClass('flipped');
+      }
+      else {
+        $('.flipped').removeClass('flipped');
+        card.addClass('flipped');
+      }
+      return false;
+    });
   }
-);
 
-// Decide whether to use css or jquery to do the card flipping
-if (Modernizr.csstransforms3d){
-  css_flip();
-}
-else {
-  jquery_flip();
-}
+  function jquery_flip(){
+    var box_width = $('.linkbox').width();
+    var margin = box_width / 2 + 'px';
+    box_width += 'px';
+
+    var compress = {
+        width: 0,
+        marginLeft: margin,
+        opacity: 0.4
+    };
+    var decompress = {
+      width: box_width,
+      marginLeft: 0,
+      opacity: 1
+    };
+
+    var css_properties = {
+      "compress":compress,
+      "decompress":decompress
+    };
+
+    $(".back").css(compress);
+    $(".back").hide();
+
+    //animate width to 0 and margin-top to 1/2 width
+    $('.flip-button').click(function(){
+      var card = $(this).parent();
+      if (card.hasClass('back')) {
+        animate_jquery_flip($('.back:visible'), css_properties);
+      }
+      else {
+        animate_jquery_flip($('.back:visible'), css_properties);
+        animate_jquery_flip(card, css_properties);
+      }
+    });
+  }
+
+  function animate_jquery_flip(card, css_properties) {
+    card.animate(css_properties["compress"], 100, function() {
+      $(this).hide();
+      // animate second card to full width and margin-top to 0
+      $(this).siblings('.linkbox').show().animate(css_properties["decompress"], 100);
+    });
+  }
+
+  // Add "hover" to the linkbox in focus
+  $(".no-touch .front").hover(
+    function(){
+      $(this).addClass("hover");
+      $(this).find(".flip-button").show();
+    },
+    function(){
+      $(this).removeClass("hover");
+      $(this).find(".flip-button").hide();
+    }
+  );
+
+  // Decide whether to use css or jquery to do the card flipping
+  if (Modernizr.csstransforms3d){
+    css_flip();
+  }
+  else {
+    jquery_flip();
+  }
+});
