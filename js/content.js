270b7cdefe4b3e875dbbe8b8dcf4540801b6c0e4
Morten Vaale Noddeland <morten@noddeland.no>
2013-08-15 16:30:29 +0200

    The cards' content should now look good in all kinds of sizes

--- js/content.js
+++ js/content.js
@@ -2,7 +2,7 @@
 var CardCtrl = ['$scope', function($scope) { /* Complicated constructor to survive minification carnage */
   var itslearning = {
     'title':'Itslearning',
-    'description':'Kanskje den viktigste siden i studenthverdagen - forelesningsfoiler, øvings-oppgaver og beskjeder fra foreleserne i emnene du tar.',
+    'description':'Kanskje den viktigste siden i hverdagen - forelesningsfoiler, øvings-oppgaver og beskjeder fra foreleserne legges her.',
     'link':'https://sats.itea.ntnu.no/sso-wrapper/web/wrapper?target=itslearning',
     'pictogram':'itslearning',
     'css_id':'itslearning'
@@ -10,7 +10,7 @@ var CardCtrl = ['$scope', function($scope) { /* Complicated constructor to survi
 
   var email = {
     'title':'Epost',
-    'description':'Du har signert på at du sjekker NTNU-mailen din jevnlig. Et tips er å videresende disse epostene til din private mailadresse - les hvordan <a href="https://innsida.ntnu.no/wiki/-/wiki/Norsk/Slik+bruker+du+webmail#section-Slik+bruker+du+webmail-Videresende+epost+til+en+annen+konto">her</a>.',
+    'description':'Du har signert på at du sjekker NTNU-mailen din jevnlig. Et tips er å videresende disse epostene til din private epostadresse - les hvordan <a href="https://innsida.ntnu.no/wiki/-/wiki/Norsk/Slik+bruker+du+webmail#section-Slik+bruker+du+webmail-Videresende+epost+til+en+annen+konto">her</a>.',
     'link':'https://webmail.stud.ntnu.no/',
     'pictogram':'email',
     'css_id':'email'
@@ -18,7 +18,7 @@ var CardCtrl = ['$scope', function($scope) { /* Complicated constructor to survi
 
   var schedule = {
     'title':'Timeplan',
-    'description':'Denne kjekke tjenesten lager timeplanen din for deg, hvis du forteller den hvilke emner du tar. Bruk gjerne NTNU-brukernavnet ditt.',
+    'description':'Denne kjekke tjenesten lager timeplanen din for deg, hvis du forteller den hvilke emner du tar. Bruk gjerne NTNU brukernavnet ditt.',
     'link':'http://ntnu.1024.no/',
     'pictogram':'schedule',
     'css_id':'schedule'
