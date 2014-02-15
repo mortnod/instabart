e8a981990f93e640de99c00fc331765f48734992
Morten Noddeland <morten@noddeland.no>
2014-02-15 14:39:38 +0100

    Replaced Angular.js with Handlebars.js. Templated the cards (but not the taglines)

--- js/content.js
+++ js/content.js
@@ -1,103 +1,80 @@
-// Card controller
-var CardCtrl = ['$scope', function($scope) { /* Complicated constructor to survive minification carnage */
-  var itslearning = {
-    'title':'Itslearning',
-    'description':'Kanskje den viktigste siden i hverdagen - forelesningsfoiler, øvings-oppgaver og beskjeder fra foreleserne legges her.',
-    'link':'https://sats.itea.ntnu.no/sso-wrapper/web/wrapper?target=itslearning',
-    'pictogram':'itslearning',
-    'css_id':'itslearning'
-  };
-
-  var email = {
-    'title':'Epost',
-    'description':'Det forventes at du sjekker NTNU-mailen din jevnlig. Et tips er å videresende disse epostene til din private epostadresse - les hvordan <a href="https://innsida.ntnu.no/wiki/-/wiki/Norsk/Slik+bruker+du+webmail#section-Slik+bruker+du+webmail-Videresende+epost+til+andre+kontoer" id="track-email-extra">her</a>.',
-    'link':'https://webmail.stud.ntnu.no/',
-    'pictogram':'email',
-    'css_id':'email'
-  };
-
-  var schedule = {
-    'title':'Timeplan',
-    'description':'Denne kjekke tjenesten lager timeplanen din for deg, hvis du forteller den hvilke emner du tar. Bruk gjerne NTNU brukernavnet ditt.',
-    'link':'http://ntnu.1024.no/',
-    'pictogram':'schedule',
-    'css_id':'schedule'
-  };
-
-  var studweb = {
-    'title':'Studweb',
-    'description':'Meld deg på (og av) emner, godkjenn studieplan, betal semesteravgift, og finn ut hvor hardt du failet på eksamen.',
-    'link':'https://idp.feide.no/simplesaml/saml2/idp/SSOService.php?SAMLRequest=fVJNj5swEL33VyD3DAbSSisrsEo3WjXSbhMtbCv1ZswATozt2iZs%2F30NJFJ6yYWDeW9m3sf68aMXwRmM5UpmKIliFIBkquayzdB7%2BRw%2BoMf809rSXmiyGVwn3%2BDPANYFG2vBOE97UtIOPZgCzJkzeH97yVDnnLYEY%2BuGGqQboYqkk0MkFWYtDysu8S%2Bo9tURmLM3qDQaFcUjxQ3wGl6V4RQFW7%2BOS%2BrmE6%2BTea2jGTTNtLzXAqYj8fRJp7%2B4KPaXkyLdaRTsthkSDISoNKvVSVPa1ae%2BaTToY9f10Ctoa6qOoj01Hm3tADtpHZUuQ2mcrML4a5g%2BlGlCkpTEq98oOBjlFFPiG5eLYYORRFHLLZG0B0scI8Xm9YWkUUyqBWTJ97I8hId9Uc4Dzl6C%2BeHRGbqVhIKf11DSKRQfk7RkEnd%2FC72mckvR9zn6IuNK%2BfANWIz2Po%2FjGI2rSJkWp3Gc4PgL9iiP%2BIzyuRZkdsrk04aeMiBXCcQu7vuVakmfO6DRFLZPeo1vyZeCTT7stgclOPsbbIRQ45MB6rw3zgyAgmdleuruq5leeB02M5Q4Q6XlvlwI58vK%2F2uc%2FwM%3D&RelayState=NTNU%26fnromgjor%3D',
-    'pictogram':'studweb',
-    'css_id':'studweb'
-  };
-
-  var dinner = {
-    'title':'Middag',
-    'description':'Hva disker din lokale SIT-kantine opp med i dag?',
-    'link':'https://www.sit.no/middag',
-    'pictogram':'dinner',
-    'css_id':'dinner'
-  };
-
-  var map = {
-    'title':'Campuskart',
-    'description':'Hjelp, hvor er datasalen Sprokkit? Hva er dette «Kjelhuset» alle snakker om? MazeMap viser vei! <br>(…på Gløshaugen / St. Olav)',
-    'link':'http://use.mazemap.com/',
-    'pictogram':'map',
-    'css_id':'map'
-  };
-
-  var filesender = {
-    'title':'FileSender',
-    'description':'Trenger du å sende en diger fil til noen? Når mailbokser og minnepinner kneler, kommer FileSender til unnsetning!',
-    'link':'https://filesender.uninett.no/simplesaml/module.php/core/as_login.php?AuthId=default-sp&ReturnTo=https://filesender.uninett.no/index.php?s=upload',
-    'pictogram':'filesender',
-    'css_id':'filesender'
-  };
-
-  var grades = {
-    'title':'Karakterer',
-    'description':'Karakterstatistikk for flere år bakover. Hvilke av emnene du tar i år er typiske stryk-emner?',
-    'link':'https://innsida.ntnu.no/sso/?target=KarstatProd',
-    'pictogram':'grades',
-    'css_id':'grades'
-  };
-
-  var romres = {
-    'title':'Romres',
-    'description':'Reservér grupperom og auditorier et halvt år i forveien. Timeplaner for enkeltrom finner du <a href="http://www.ntnu.no/studieinformasjon/rom/" id="track-romres-extra">her</a>.',
-    'link':'https://romres.ntnu.no/',
-    'pictogram':'romres',
-    'css_id':'romres'
-  };
-
-  var ithjelp = {
-    'title':'IT-hjelp',
-    'description':'Hvordan koble telefonen automatisk til eduroam? Hvordan bruke printerne på campus? NTNUs IT-hjelp løser dine problemer!',
-    'link':'https://innsida.ntnu.no/it-hjelp',
-    'pictogram':'it-help',
-    'css_id':'it-hjelp'
-  };
-
-  var farm = {
-    'title':'Farm',
-    'description':'Savner du Word? Farm gir deg tilgang til programmene, filene og nettsidene du ville hatt på en datasal. Kort innføring finner du <a href="https://innsida.ntnu.no/wiki/-/wiki/Norsk/Programfarm" id="track-farm-extra">her</a>.',
-    'link':'https://farm.ntnu.no/',
-    'pictogram':'farm',
-    'css_id':'farm'
-  };
-
-  var kundesenteret = {
-    'title':'Grupper',
-    'description':'Lag dine egne epostlister, og opprett grupper for dine NTNU-prosjekter, der du kan dele filer med andre.',
-    'link':'https://innsida.ntnu.no/sso/?target=studdrift_groups',
-    'pictogram':'kundesenteret',
-    'css_id':'kundesenteret'
-  };
-
-  $scope.cards = [itslearning, email, schedule, studweb, dinner, map, filesender, grades, romres, ithjelp, farm, kundesenteret];
-}];
+var data = {
+  cards: [
+    {
+      title:'Itslearning',
+      pictogram:'itslearning',
+      css_id:'itslearning',
+      link:'https://sats.itea.ntnu.no/sso-wrapper/web/wrapper?target=itslearning',
+      description: 'Kanskje den viktigste siden i hverdagen - forelesningsfoiler, øvings-oppgaver og beskjeder fra foreleserne legges her.'
+    }, {
+      title:'Epost',
+      pictogram:'email',
+      css_id:'email',
+      link:'https://webmail.stud.ntnu.no/',
+      description:'Det forventes at du sjekker NTNU-mailen din jevnlig. Et tips er å videresende disse epostene til din private epostadresse - les hvordan <a href="https://innsida.ntnu.no/wiki/-/wiki/Norsk/Slik+bruker+du+webmail#section-Slik+bruker+du+webmail-Videresende+epost+til+andre+kontoer" id="track-email-extra">her</a>.'
+    }, {
+      title:'Timeplan',
+      pictogram:'schedule',
+      css_id:'schedule',
+      link:'http://ntnu.1024.no/',
+      description:'Denne kjekke tjenesten lager timeplanen din for deg, hvis du forteller den hvilke   emner du tar. Bruk gjerne NTNU brukernavnet ditt.'
+    }, {
+      title:'Studweb',
+      pictogram:'studweb',
+      css_id:'studweb',
+      link:'https://idp.feide.no/simplesaml/saml2/idp/SSOService.php?SAMLRequest=fVJNj5swEL33VyD3DAbSSisrsEo3WjXSbhMtbCv1ZswATozt2iZs%2F30NJFJ6yYWDeW9m3sf68aMXwRmM5UpmKIliFIBkquayzdB7%2BRw%2BoMf809rSXmiyGVwn3%2BDPANYFG2vBOE97UtIOPZgCzJkzeH97yVDnnLYEY%2BuGGqQboYqkk0MkFWYtDysu8S%2Bo9tURmLM3qDQaFcUjxQ3wGl6V4RQFW7%2BOS%2BrmE6%2BTea2jGTTNtLzXAqYj8fRJp7%2B4KPaXkyLdaRTsthkSDISoNKvVSVPa1ae%2BaTToY9f10Ctoa6qOoj01Hm3tADtpHZUuQ2mcrML4a5g%2BlGlCkpTEq98oOBjlFFPiG5eLYYORRFHLLZG0B0scI8Xm9YWkUUyqBWTJ97I8hId9Uc4Dzl6C%2BeHRGbqVhIKf11DSKRQfk7RkEnd%2FC72mckvR9zn6IuNK%2BfANWIz2Po%2FjGI2rSJkWp3Gc4PgL9iiP%2BIzyuRZkdsrk04aeMiBXCcQu7vuVakmfO6DRFLZPeo1vyZeCTT7stgclOPsbbIRQ45MB6rw3zgyAgmdleuruq5leeB02M5Q4Q6XlvlwI58vK%2F2uc%2FwM%3D&RelayState=NTNU%26fnromgjor%3D',
+      description:'Meld deg på (og av) emner, godkjenn studieplan, betal semesteravgift, og finn ut hvor hardt du failet på eksamen.'
+    }, {
+      title:'Middag',
+      pictogram:'dinner',
+      css_id:'dinner',
+      link:'https://www.sit.no/middag',
+      description:'Hva disker din lokale SIT-kantine opp med i dag?'
+    }, {
+      title:'Campuskart',
+      pictogram:'map',
+      css_id:'map',
+      link:'http://use.mazemap.com/',
+      description:'Hjelp, hvor er datasalen Sprokkit? Hva er dette «Kjelhuset» alle snakker om? MazeMap  viser vei! <br>(…på Gløshaugen / St. Olav)'
+    }, {
+      title:'FileSender',
+      pictogram:'filesender',
+      css_id:'filesender',
+      link:'https://filesender.uninett.no/simplesaml/module.php/core/as_login.php?AuthId=default-  sp&ReturnTo=https://filesender.uninett.no/index.php?s=upload',
+      description:'Trenger du å sende en diger fil til noen? Når mailbokser og minnepinner kneler, kommer  FileSender til unnsetning!'
+    }, {
+      title:'Karakterer',
+      pictogram:'grades',
+      css_id:'grades',
+      link:'https://innsida.ntnu.no/sso/?target=KarstatProd',
+      description:'Karakterstatistikk for flere år bakover. Hvilke av emnene du tar i år er typiske stryk- emner?'
+    }, {
+      title:'Romres',
+      pictogram:'romres',
+      css_id:'romres',
+      link:'https://romres.ntnu.no/',
+      description:'Reservér grupperom og auditorier et halvt år i forveien. Timeplaner for enkeltrom   finner du <a href="http://www.ntnu.no/studieinformasjon/rom/" id="track-romres-extra">her</a>.'
+    }, {
+      title:'IT-hjelp',
+      pictogram:'it-help',
+      css_id:'it-hjelp',
+      link:'https://innsida.ntnu.no/it-hjelp',
+      description:'Hvordan koble telefonen automatisk til eduroam? Hvordan bruke printerne på campus? NTNUs IT-hjelp løser dine problemer!'
+    }, {
+      title:'Farm',
+      pictogram:'farm',
+      css_id:'farm',
+      link:'https://farm.ntnu.no/',
+      description:'Savner du Word? Farm gir deg tilgang til programmene, filene og nettsidene du ville hatt på en datasal. Kort innføring finner du <a href="https://innsida.ntnu.no/wiki/-/wiki/Norsk/Programfarm" id="track-farm-extra">her</a>.'
+    }, {
+      title:'Grupper',
+      pictogram:'kundesenteret',
+      css_id:'kundesenteret',
+      link:'https://innsida.ntnu.no/sso/?target=studdrift_groups',
+      description:'Lag dine egne epostlister, og opprett grupper for dine NTNU-prosjekter, der du kan dele filer med andre.'
+    }
+  ]
+};
 
 // Tagline controller
 var TaglineCtrl = ['$scope', function($scope) { /* Complicated constructor to survive minification carnage */
