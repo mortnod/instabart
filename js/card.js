674c7bdac34ed24235b42f49c50049b34bcdb56e
Morten Noddeland <morten@noddeland.no>
2014-02-16 23:42:36 +0100

    Refactored the card flip code. Deleted 'cardflip.js' and placed the functionality within 'card.js'

--- js/card.js
+++ js/card.js
@@ -2,7 +2,12 @@ var s,
 Card = {
 
   settings: {
-    cards: null
+    cards: null,
+    infoBtn: null,
+    jqueryFlip: {
+      shrink: null,
+      expand: null
+    }
   },
 
   init: function() {
@@ -10,6 +15,15 @@ Card = {
     this.createCards(function(){
       // Initialize some settings variables
       s.cards = $('.card');
+      s.infoBtn = $('.flip-button');
+
+      // jQuery flip setup
+      if (!Modernizr.csstransforms3d || !Modernizr.csstransformspreserve3d){
+        Card.setJQueryFlipSettings();
+        $(".back")
+          .css(s.jqueryFlip.shrink)
+          .hide();
+      }
 
       Card.bindUIActions();
       Card.resizeCards();
@@ -18,7 +32,7 @@ Card = {
 
   createCards: function(callback) {
     var template = Handlebars.templates['card'];
-    for (i = 0; i < data.cards.length; i++) {
+    for (var i = 0; i < data.cards.length; i++) {
       var html = template(data.cards[i]);
       $('#grid').append(html);
     }
@@ -28,10 +42,28 @@ Card = {
 
   bindUIActions: function(){
     $(window).resize(function() {
+      // Resize the cards if the browser window is resized
       Card.resizeCards();
+
+      // Insert intelligent comment here
+      if (!Modernizr.csstransforms3d || !Modernizr.csstransformspreserve3d){
+        Card.setJQueryFlipSettings();
+      }
     });
 
+    // Animation when a card is clicked
     Card.cardClickedAnimation();
+
+    // Flip card by clicking the info button
+    s.infoBtn.click(function(){
+      if (Modernizr.csstransforms3d && Modernizr.csstransformspreserve3d){
+        Card.flipCardWithCSS($(this));
+
+      } else {
+        Card.flipCardWithJQuery($(this));
+      }
+    });
+
   },
 
   resizeCards: function(){
@@ -50,4 +82,63 @@ Card = {
       $(this).removeClass('active');
     });
   },
+
+  flipCardWithCSS: function(infoBtn){
+    // Display the back of the cards (hidden by default to prevent flashing on load)
+    $(".back").show();
+
+    var cardContent = infoBtn.parents('.content');
+
+    // If this card is already flipped, then just flip it back
+    if (cardContent.hasClass('flipped')){
+      cardContent.removeClass('flipped');
+    }
+    // Else, flip all cards that have already been flipped, then flip this card
+    else {
+      $('.flipped').removeClass('flipped');
+      cardContent.addClass('flipped');
+    }
+    return false;
+  },
+
+  flipCardWithJQuery: function(infoBtn){
+    var cardFace = infoBtn.parent();
+    var previouslyFlippedCard = $('.back:visible');
+
+    if (cardFace.hasClass('back')) {
+      Card.animateJQueryFlip(previouslyFlippedCard);
+    }
+    else {
+      Card.animateJQueryFlip(previouslyFlippedCard);
+      Card.animateJQueryFlip(cardFace);
+    }
+  },
+
+  animateJQueryFlip: function(cardFace) {
+    var otherCardFace = cardFace.siblings('.cardface');
+
+    cardFace.animate(s.jqueryFlip.shrink, 100, function() {
+
+      cardFace.hide();
+      otherCardFace.show();
+
+      otherCardFace.animate(s.jqueryFlip.expand, 100, function(){
+        $(this).width('');
+      });
+    });
+  },
+
+  setJQueryFlipSettings: function() {
+    s.jqueryFlip.shrink = {
+      width: 0,
+      marginLeft: s.cards.width() / 2 + 'px',
+      opacity: 0.4
+    };
+
+    s.jqueryFlip.expand = {
+      width: s.cards.width() + 'px',
+      marginLeft: 0,
+      opacity: 1
+    };
+  }
 };
