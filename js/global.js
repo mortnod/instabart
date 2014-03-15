eaa05624f3f93367f0a4889401f03b071e04b68f
Morten Noddeland <morten@noddeland.no>
2014-03-15 21:45:30 +0100

    Small fix for iOS 7.1 devices

--- js/global.js
+++ js/global.js
@@ -12,7 +12,7 @@
 
   // @if ENV='phone'
   function fixStatusBarIniOS7() {
-    if (window.device.platform === 'iOS' && parseFloat(window.device.version) === 7.0){
+    if (window.device.platform === 'iOS' && parseFloat(window.device.version) >= 7.0){
       StatusBar.overlaysWebView(false);
       StatusBar.backgroundColorByHexString('#777777');
     }
