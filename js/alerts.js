034e720ac36cb6a8bb6db99761e8702af8151356
Morten Noddeland <morten@noddeland.no>
2014-09-06 18:04:52 +0200

    If the user closes the alert message, it will now remain closed

--- js/alerts.js
+++ js/alerts.js
@@ -1,14 +1,34 @@
 Alerts = {
 
   init: function() {
+    if (Schedule.supportsLocalStorage()) {
+      if(this.shouldAlertBeDisplayed()) {
+        this.displayAlert();
+      }
+    }
+  },
+
+  shouldAlertBeDisplayed: function() {
+    if (localStorage['alert_closed'] === undefined) {
+      return true;
+    }
+    else {
+      return !JSON.parse(localStorage['alert_closed']);
+    }
+  },
+
+  displayAlert: function() {
+    $('.alert').show();
+    $('#grid').addClass('alert-padding');
     Alerts.bindCloseButton();
   },
 
   bindCloseButton: function() {
     $('.alert .close').click(function(){
+      localStorage['alert_closed'] = 'true';
       $('.alert').fadeOut(function() {
         $('#grid').removeClass('alert-padding');
       });
     });
-  }
+  },
 };
