40f5267cf759667b0e6d9fcd7470cbfe1b068f06
Michael McMillan <email@michaelmcmillan.net>
2014-12-17 09:53:06 +0100

    added links.js to Gruntfile for processing

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,6 +31,7 @@ module.exports = function(grunt) {
           'js/build/processed/global.js' : 'js/global.js',
           'js/build/processed/schedule.js' : 'js/schedule.js',
           'js/build/processed/content.js' : 'js/content.js',
+          'js/build/processed/links.js': 'js/links.js',
         }
       },
       web : {
@@ -43,6 +44,7 @@ module.exports = function(grunt) {
           'js/build/processed/global.js' : 'js/global.js',
           'js/build/processed/schedule.js' : 'js/schedule.js',
           'js/build/processed/content.js' : 'js/content.js',
+          'js/build/processed/links.js': 'js/links.js',
         }
       }
     },
@@ -62,6 +64,7 @@ module.exports = function(grunt) {
           'js/build/processed/analytics.js',
           'js/header.js',
           'js/alerts.js',
+          'js/links.js',
           'js/build/processed/global.js'
         ],
         dest: 'js/build/production.js',
