ce14af8e109356744011d4b515dd57d514df69a2
Morten Noddeland <morten@noddeland.no>
2024-03-04 17:46:47 +0100

    Change to white background with slightly gray cards

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,8 +10,7 @@ export default {
         xl: '1270px', // 6 cards per column (fixed width)
       },
       boxShadow: {
-        DEFAULT:
-          '0 1px 3px 0 rgba(0, 0, 0, 0.05), 0 1px 1px 0 rgba(0, 0, 0, 0.15)',
+        card: '0 0 0 1px rgba(0, 0, 0, 0.10), 0 2px 5px 0 rgba(0, 0, 0, 0.10)',
       },
     },
   },
